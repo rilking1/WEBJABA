789ab479ecf2ce68470ff0e5e8e602c372a4ffc2
rilking1 <kozakevycr2@gmail.com>
2024-09-28 12:11:57 +0300

    Добавьте файлы проекта.

---
+++ WEBJABA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEBJABA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
